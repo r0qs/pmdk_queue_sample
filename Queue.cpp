76f77adb0096c66ca9f10f7e3e17f671e970edaf
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2018-05-29 17:01:11 -0300

    Clean up code

--- Queue.cpp
+++ Queue.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <string>
 #include <libpmemobj++/make_persistent.hpp>
-#include <libpmemobj++/p.hpp>
 #include <libpmemobj++/persistent_ptr.hpp>
 #include <libpmemobj++/pool.hpp>
 #include <libpmemobj++/transaction.hpp>
